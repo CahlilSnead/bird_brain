317e6f6e796eec07c7650ad8d00fba7dce937afc
Cahlil Snead <cahlilsnead@Cahlils-MacBook-Pro.local>
2021-11-01 19:46:16 -0700

    modified lesson_1.py

--- BirdBrainPython/lesson_1.py
+++ BirdBrainPython/lesson_1.py
@@ -1,7 +1,25 @@
 from BirdBrain import Finch
 bird = Finch()
 
+'''
 print('exercise 1')
 bird.setMove('F',20,50)
 
 print('exercise 2')
+bird.setMove('F',25,100)
+bird.setMove('B',25,30)
+
+print('exercise 3')
+bird.setTurn('R',360,50)
+bird.setTurn('L',360,60)
+'''
+print('exercise 4')
+bird.setMove('F',20,70)
+bird.setTurn('R',90,70)
+bird.setMove('F',20,70)
+bird.setTurn('R',90,70)
+bird.setMove('F',20,70)
+bird.setTurn('R',90,70)
+bird.setMove('F',20,70)
+bird.setTurn('R',90,70)
+
